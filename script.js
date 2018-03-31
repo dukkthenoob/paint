5ba8a242b43c595d14fda6ae254a1d9eec9fa030
Alex <mistahdukk@gmail.com>
2018-03-31 20:06:55 +0100

    added selecting to tools and sizes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+$(function(){
+    var size = $("#sizes");
+    var tools = $(".tool-bar").children().children();
+
+    size.on("click",function(e){
+        var target = $(e.target);
+        if (target.is("li")){
+            target.addClass("size-active").siblings().removeClass("size-active");
+        }
+    });
+
+    tools.on("click",function(e){
+        var target = $(e.target);
+        if(target.is("i")){
+            target.addClass("tool-active").siblings().removeClass("tool-active");
+        }
+    });
+
+});
